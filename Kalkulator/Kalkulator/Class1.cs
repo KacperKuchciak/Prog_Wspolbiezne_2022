978a1c643eca581320ab789ed163c363fe0eeb03
MikolajDzikowski <236523@edu.p.lodz.pl>
2022-03-16 21:30:58 +0100

    Podstawowy program

---
+++ Kalkulator/Kalkulator/Class1.cs
@@ -0,0 +1,35 @@
+﻿namespace Kalkulator
+{
+    public abstract class Calculator
+    {
+
+        public Calculator()
+        {
+        }
+
+        private int counter = 0;
+        public int Add(int x, int y)
+        {
+            counter++;
+            return x + y;
+        }
+        
+        public int Subtract(int x, int y)
+        {
+            counter++;
+            return x - y;
+        }
+        
+        public int Multiply(int x, int y)
+        {
+            counter++;
+            return x * y; 
+        }
+        
+        public double Divide(int x, int y)
+        {
+            counter++;
+            return x / y;
+        }
+    }
+}
